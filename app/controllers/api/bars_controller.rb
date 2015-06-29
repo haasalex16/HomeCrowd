a9dbc3fe211443cd90221a76de48852804aa2500
Alex Haas <Alex.carl.Haas@gmail.com>
2015-06-29 16:35:38 -0400

    Create bar controller

---
+++ app/controllers/api/bars_controller.rb
@@ -0,0 +1,39 @@
+class Api::BarsController < ApplicationController
+
+  wrap_parameters false
+
+  def create
+    @bar = Bar.new(bar_params)
+    if @bar.save
+      render json: @bar
+    else
+      render json: @bar.errors, status: 422
+    end
+  end
+
+  def show
+    @bar = Bar.find(params[:id])
+    render json: @bar
+  end
+
+  def index
+    @bars = Bar.all
+    render :index
+  end
+
+  def destroy
+  end
+
+  private
+    def bar_params
+      params.require(:bar).permit(:name,
+                                  :address,
+                                  :number,
+                                  :website,
+                                  :notes,
+                                  :alumni,
+                                  :longitude,
+                                  :latitude)
+    end
+
+end
