11e6a0e0470bcfccf4a50fd0e913922d688cf4b5
Alex Haas <alex.carl.haas@gmail.com>
2015-07-03 02:57:04 -0400

    Add all school seeds and markers

--- app/assets/javascripts/views/homeshow.js
+++ app/assets/javascripts/views/homeshow.js
@@ -2,8 +2,164 @@ HomeCrowd.Views.HomeShow = Backbone.View.extend ({
   template: JST['home/show'],
 
   events: {
-    'click #addMarkers': 'addMarkers',
+    'click #addUMMarkers': 'addUM',
+    'click #addIUMarkers': 'addIU',
+    'click #addUIMarkers': 'addUI',
+    'click #addMSUMarkers': 'addMSU',
+    'click #addNUMarkers': 'addNU',
+    'click #addOSUMarkers': 'addOSU',
+    'click #addPSUMarkers': 'addPSU',
+    'click #addPUMarkers': 'addPU',
+    'click #addRMarkers': 'addR',
+    'click #addUIOWAMarkers': 'addIOWA',
+    'click #addMDMarkers': 'addMD',
+    'click #addUNMarkers': 'addUN',
+    'click #addUWMarkers': 'addUW',
+    'click #addMINNMarkers': 'addMINN',
     'click #removeMarkers': 'removeMarkers'
+  },
+
+  addMINN: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Minnesota') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/135.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addUW: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Wisconsin') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/275.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addUN: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Nebraska') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/158.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addMD: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Maryland') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/120.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addIOWA: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Iowa') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/2294.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addR: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'Rutgers') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/164.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addPU: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'Purdue University') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/2509.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addPSU: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'Penn State University') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/213.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addOSU: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'Ohio State University') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/194.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addNU: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'Northwestern University') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/77.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addMSU: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'Michigan State University') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/127.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
   },
 
   initialize: function () {
@@ -23,28 +179,58 @@ HomeCrowd.Views.HomeShow = Backbone.View.extend ({
     for (var i = 0; i < this.markers.length; i++) {
       this.markers[i].setMap(null);
     }
+    this.markers = [];
   },
 
-  addMarkers: function () {
+  addIU: function () {
+    this.removeMarkers();
     this.collection.models.forEach(function(model) {
-      var contentString = "<div>" + model.get('name') + "</div>"
-
-      var lat = model.get('lat');
-      var lng = model.get('lng');
-      this.addMarker(lat, lng, contentString);
+      if (model.get('loyalty') === 'University of Indiana') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/84.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
     }.bind(this))
-
   },
 
-  addMarker: function (lat, lng, contentString) {
+  addUM: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Michigan') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/130.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addUI: function () {
+    this.removeMarkers();
+    this.collection.models.forEach(function(model) {
+      if (model.get('loyalty') === 'University of Illinois') {
+        var contentString = "<div>" + model.get('name') + "</div>"
+        var lat = model.get('lat');
+        var lng = model.get('lng');
+        var image_url = 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/356.png&w=80&h=80&transparent=true';
+        this.addMarker(lat, lng, contentString, image_url);
+      }
+    }.bind(this))
+  },
+
+  addMarker: function (lat, lng, contentString, image_url) {
     var myLatlng = new google.maps.LatLng(lat,lng);
     var infowindow = new google.maps.InfoWindow({
         content: contentString
     });
     var marker = new google.maps.Marker({
+        animation:  google.maps.Animation.DROP,
         map: this.map,
         icon: {
-          url: 'http://a.espncdn.com/combiner/i?img=/i/teamlogos/ncaa/500/130.png&w=80&h=80&transparent=true',
+          url: image_url,
           scaledSize: {
             width: 30,
             height: 30
@@ -52,7 +238,6 @@ HomeCrowd.Views.HomeShow = Backbone.View.extend ({
         },
         // icon: 'images/mich.png',
         position: myLatlng,
-        animation:  google.maps.Animation.DROP,
     });
     this.markers.push(marker);
 
