ba9645aca2c3b0fab124ab17ae074277c085b136
Alex Haas <alex.carl.haas@gmail.com>
2015-07-23 03:10:20 -0400

    Sort collection by name

--- app/assets/javascripts/collections/bars.js
+++ app/assets/javascripts/collections/bars.js
@@ -3,6 +3,10 @@ HomeCrowd.Collections.Bars = Backbone.Collection.extend({
 
   model: HomeCrowd.Models.Bar,
 
+  comparator: function(model){
+    return model.get('name');
+  }
+
 });
 
 HomeCrowd.Collections.bars = new HomeCrowd.Collections.Bars ();
