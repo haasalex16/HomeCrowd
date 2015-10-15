1ade50ee71de32146bf863e039bb5de51c6f3f7b
Alex Haas <alex.carl.haas@gmail.com>
2015-10-15 18:14:02 -0400

    Add autocomplete

--- app/assets/javascripts/views/loyalties/loyalty_form.js
+++ app/assets/javascripts/views/loyalties/loyalty_form.js
@@ -2,10 +2,31 @@ HomeCrowd.Views.LoyaltiesForm = Backbone.View.extend({
 
   template: JST['loyalties/form'],
 
+  events: {
+    'submit form': 'submit'
+  },
+
   render: function() {
     var view = this.template();
     this.$el.html(view);
+    this.initAuto();
     return this;
+  },
+
+  submit: function(event) {
+    event.preventDefault();
+    console.log(event);
+  },
+
+  initAuto: function () {
+    var input = this.$('#autocomp')[0];
+
+    var autocomplete = new google.maps.places.Autocomplete(input);
+    autocomplete.addListener('place_changed', function() {
+      var place = autocomplete.getPlace();
+      console.log(place.place_id);
+    });
   }
+
 
 });
